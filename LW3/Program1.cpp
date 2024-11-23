f32debaa4fd8645aa8daab096a51d4f8685fec06
Elik565 <eliseysavich@gmail.com>
2024-11-23 19:11:46 +0300

    Отделил программу 1

--- LW3/Program1.cpp
+++ LW3/Program1.cpp
@@ -1,11 +1,23 @@
-#include "Program1.hpp"
 #include <fstream>
 #include <iostream>
 
-void read_file(const char* FILE) {
+void read_and_output_file(const std::string& file_path, const std::string& fifo_path) {
     std::ifstream fin(FILE);
 
     if (!fin) {
-        std::cout << "Файл не найден\n";
+        std::cout << "Ошибка при открытии файла\n";
     }
+
+    
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        std::cout << "Неправильные входные парметры для программы 1\n";
+        exit(1);
+    }
+
+    read_and_output_file(argv[1], argv[2]);
+
+    return 0;
 }
--- LW3/main.cpp
+++ LW3/main.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-#include "Program1.hpp"
 
 int main() {
-    const char* FILE = "input_file.txt";  // входной файл
-
-    read_file(FILE);
+    
 
     return 0;
 }

9ef0d53a5e512adbe632d329a8e493ab83bf4968
Elik565 <eliseysavich@gmail.com>
2024-12-07 16:27:59 +0300

    Настройка подключения dbus

--- LW6/main.cpp
+++ LW6/main.cpp
@@ -11,8 +11,31 @@ std::string get_file_extension(const std::string& file_path) {
     return file_path.substr(dot_pos + 1);
 }
 
-void open_file_with_dbus(const std::string& file_path) {
+void open_file_with_dbus(const std::string& file_path, const std::string& extension) {
+    // инициализация 
+    DBusError err;
+    DBusConnection* conn;
+    dbus_error_init(&err);
 
+    // подключение к системной шине dbus
+    conn = dbus_bus_get(DBUS_BUS_SESSION, &err);
+    if (dbus_error_is_set(&err)) {
+        std::cerr << "Ошибка подключения к D-Bus: " << err.message << "\n";
+        dbus_error_free(&err);
+        exit(1);
+    }
+
+    if (!conn) {
+        std::cerr << "Не удалось подключиться к D-Bus\n";
+        exit(1);
+    }
+
+    DBusMessage* msg = dbus_message_new_method_call(
+        "org.freedesktop.FileService", // имя сервиса
+        "/org/freedesktop/FileService", // объект
+        "org.freedesktop.FileService",  // интерфейс
+        "OpenFile"  // метод
+    );
 }
 
 int main(int argc, char* argv[]) {
@@ -27,6 +50,8 @@ int main(int argc, char* argv[]) {
         std::cerr << "Не удалось определить расширение файла\n";
         return 1;
     }
+
+    open_file_with_dbus(argv[1], extension);
 
     return 0;
 }
